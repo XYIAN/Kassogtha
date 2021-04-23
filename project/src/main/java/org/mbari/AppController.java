f02313e8960487088ba77fc483d4ef6f53547b59
XYIAN <kdilbeck@csumb.edu>
2021-04-23 12:04:12 -0700

    update meeting

--- project/src/main/java/org/mbari/AppController.java
+++ project/src/main/java/org/mbari/AppController.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.*;
 
 
 import com.google.gson.Gson;
@@ -139,11 +140,89 @@ public class AppController {
     }
 
     public void auto(Localization loc){
-        //var conceptJson = io.getController().getLocalizations();
-        var conceptJson = io.getController().getConcept();
+        //Path conceptList = Paths.get("/home/xyian/dev/Kassogtha/project/src/main/resources/concepts.json");
+        //var conceptJson = "/home/xyian/dev/Kassogtha/project/src/main/resources/concepts.json";
+        //var conceptJson = io.getController().getConcept();
+        //log.debug("concept to convert::::-->", conceptJson);
+        //File conceptList = new File 
+        List<String> conceptList = new ArrayList<>();
+        var url = getClass().getResource("/concepts.json");
+        BufferedReader reader = null; 
+        try{
+            var t = url.openStream(); 
+            reader = new BufferedReader(new InputStreamReader(t));
+            String line; 
+            while((line = reader.readLine()) != null){
+                conceptList.add(line); 
+            }
+        } catch(IOException e){
+            e.printStackTrace(); 
+        } finally {
+            if(reader != null)
+            reader.close(); 
+        }
+        log.debug("concept to convert::::-->", conceptList);
+
         //convert JSON concept list to string array 
-        Gson gson = new GsonBuilder();
+        //Gson gson = new GsonBuilder();
         //var concepts = gson.fromJson(stringOfJson, String[].class);
-        var concepts = gson.fromJson(conceptJson, String[].class);
+        //var concepts = gson.fromJson(conceptList, String[].class);
     }
+    /*
+    @param <T>
+    public class conceptAuto<T>{
+        private ComboBox<T> cmb;
+        String filter = "";
+        private ObservableList<T> originalItems;
+    
+        public ComboBoxAutoComplete(ComboBox<T> cmb) {
+            this.cmb = cmb;
+            originalItems = FXCollections.observableArrayList(cmb.getItems());
+            cmb.setTooltip(new Tooltip());
+            cmb.setOnKeyPressed(this::handleOnKeyPressed);
+            cmb.setOnHidden(this::handleOnHiding);
+            return; 
+        }
+    
+        public void handleOnKeyPressed(KeyEvent e) {
+            ObservableList<T> filteredList = FXCollections.observableArrayList();
+            KeyCode code = e.getCode();
+    
+            if (code.isLetterKey()) {
+                filter += e.getText();
+            }
+            if (code == KeyCode.BACK_SPACE && filter.length() > 0) {
+                filter = filter.substring(0, filter.length() - 1);
+                cmb.getItems().setAll(originalItems);
+            }
+            if (code == KeyCode.ESCAPE) {
+                filter = "";
+            }
+            if (filter.length() == 0) {
+                filteredList = originalItems;
+                cmb.getTooltip().hide();
+            } else {
+                Stream<T> itens = cmb.getItems().stream();
+                String txtUsr = filter.toString().toLowerCase();
+                itens.filter(el -> el.toString().toLowerCase().contains(txtUsr)).forEach(filteredList::add);
+                cmb.getTooltip().setText(txtUsr);
+                Window stage = cmb.getScene().getWindow();
+                double posX = stage.getX() + cmb.getBoundsInParent().getMinX();
+                double posY = stage.getY() + cmb.getBoundsInParent().getMinY();
+                cmb.getTooltip().show(stage, posX, posY);
+                cmb.show();
+            }
+            cmb.getItems().setAll(filteredList);
+        }
+    
+        public void handleOnHiding(Event e) {
+            filter = "";
+            cmb.getTooltip().hide();
+            T s = cmb.getSelectionModel().getSelectedItem();
+            cmb.getItems().setAll(originalItems);
+            cmb.getSelectionModel().select(s);
+        }
+    
+    }//end autoComplet
+*/
 }
