65ba354019dbee44e55e58418fff261e21b335f3
XYIAN <kdilbeck@csumb.edu>
2021-04-28 10:00:57 -0700

    new branch link

--- project/src/main/java/org/mbari/AppController.java
+++ project/src/main/java/org/mbari/AppController.java
@@ -210,59 +210,59 @@ public class AppController {
     
 
 
-    // @param <T>
-    // public class conceptAuto<T>{
-    //     private ComboBox<T> cmb;
-    //     String filter = "";
-    //     private ObservableList<T> originalItems;
+    @param <T>
+    public class conceptAuto<T>{
+        private ComboBox<T> cmb;
+        String filter = "";
+        ObservableList<T> originalItems ; 
     
-    //     public ComboBoxAutoComplete(ComboBox<T> cmb) {
-    //         this.cmb = cmb;
-    //         originalItems = FXCollections.observableArrayList(cmb.getItems());
-    //         cmb.setTooltip(new Tooltip());
-    //         cmb.setOnKeyPressed(this::handleOnKeyPressed);
-    //         cmb.setOnHidden(this::handleOnHiding);
-    //         return; 
-    //     }
+        public ComboBoxAutoComplete(ComboBox<T> cmb) {
+            this.cmb = cmb;
+            originalItems = FXCollections.observableArrayList(cmb.getItems());
+            cmb.setTooltip(new Tooltip());
+            cmb.setOnKeyPressed(this::handleOnKeyPressed);
+            cmb.setOnHidden(this::handleOnHiding);
+            return; 
+        }
     
-    //     public void handleOnKeyPressed(KeyEvent e) {
-    //         ObservableList<T> filteredList = FXCollections.observableArrayList();
-    //         KeyCode code = e.getCode();
+        public void handleOnKeyPressed(KeyEvent e) {
+            ObservableList<T> filteredList = FXCollections.observableArrayList();
+            KeyCode code = e.getCode();
     
-    //         if (code.isLetterKey()) {
-    //             filter += e.getText();
-    //         }
-    //         if (code == KeyCode.BACK_SPACE && filter.length() > 0) {
-    //             filter = filter.substring(0, filter.length() - 1);
-    //             cmb.getItems().setAll(originalItems);
-    //         }
-    //         if (code == KeyCode.ESCAPE) {
-    //             filter = "";
-    //         }
-    //         if (filter.length() == 0) {
-    //             filteredList = originalItems;
-    //             cmb.getTooltip().hide();
-    //         } else {
-    //             Stream<T> itens = cmb.getItems().stream();
-    //             String txtUsr = filter.toString().toLowerCase();
-    //             itens.filter(el -> el.toString().toLowerCase().contains(txtUsr)).forEach(filteredList::add);
-    //             cmb.getTooltip().setText(txtUsr);
-    //             Window stage = cmb.getScene().getWindow();
-    //             double posX = stage.getX() + cmb.getBoundsInParent().getMinX();
-    //             double posY = stage.getY() + cmb.getBoundsInParent().getMinY();
-    //             cmb.getTooltip().show(stage, posX, posY);
-    //             cmb.show();
-    //         }
-    //         cmb.getItems().setAll(filteredList);
-    //     }
+            if (code.isLetterKey()) {
+                filter += e.getText();
+            }
+            if (code == KeyCode.BACK_SPACE && filter.length() > 0) {
+                filter = filter.substring(0, filter.length() - 1);
+                cmb.getItems().setAll(originalItems);
+            }
+            if (code == KeyCode.ESCAPE) {
+                filter = "";
+            }
+            if (filter.length() == 0) {
+                filteredList = originalItems;
+                cmb.getTooltip().hide();
+            } else {
+                Stream<T> itens = cmb.getItems().stream();
+                String txtUsr = filter.toString().toLowerCase();
+                itens.filter(el -> el.toString().toLowerCase().contains(txtUsr)).forEach(filteredList::add);
+                cmb.getTooltip().setText(txtUsr);
+                Window stage = cmb.getScene().getWindow();
+                double posX = stage.getX() + cmb.getBoundsInParent().getMinX();
+                double posY = stage.getY() + cmb.getBoundsInParent().getMinY();
+                cmb.getTooltip().show(stage, posX, posY);
+                cmb.show();
+            }
+            cmb.getItems().setAll(filteredList);
+        }
     
-    //     public void handleOnHiding(Event e) {
-    //         filter = "";
-    //         cmb.getTooltip().hide();
-    //         T s = cmb.getSelectionModel().getSelectedItem();
-    //         cmb.getItems().setAll(originalItems);
-    //         cmb.getSelectionModel().select(s);
-    //     }
+        public void handleOnHiding(Event e) {
+            filter = "";
+            cmb.getTooltip().hide();
+            T s = cmb.getSelectionModel().getSelectedItem();
+            cmb.getItems().setAll(originalItems);
+            cmb.getSelectionModel().select(s);
+        }
     
-    // }//end autoComplet
+    }//end autoComplet
 }
